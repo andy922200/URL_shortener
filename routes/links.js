735a5189997fa450cd28f3fa31a61daf7536cce4
andysml <andy922200@gmail.com>
2019-07-27 21:07:08 +0100

    "ADD": validation repeat link and match with the database

--- routes/links.js
+++ routes/links.js
@@ -1,9 +1,10 @@
 //initialize router
 const express = require('express')
 const router = express.Router()
+const Link = require('../models/link.js')
 const { check, validationResult } = require('express-validator')
 //const { formCheck, registerFormCheck } = require('../models/validationRule')
-const Link = require('../models/link.js')
+
 
 // send original Link to tinyURL proxy
 router.post('/', (req, res) => {
@@ -13,17 +14,33 @@ router.post('/', (req, res) => {
   let originalLink = req.body.originalLink
   let shortURL = ''
   shortURL = baseURL + randomString
-  console.log(originalLink)
-  console.log(randomString)
-  console.log(shortURL)
   const link = Link({
     link: originalLink,
     shortLink: shortURL
   })
-  link.save(err => {
-    if (err) return console.log(err)
-    res.render('result', { shortURL: shortURL, originalLink: originalLink })
+
+  Link.find({ "shortLink": shortURL }, (err, results) => {
+    if (err) return err
+    // validate repeat shortURL
+    if (results.length > 0) {
+      randomString = Math.random().toString(36).slice(7, 12)
+      shortURL = baseURL + randomString
+    }
+    Link.find({ "link": originalLink }, (err, results) => {
+      if (err) return err
+      // validate repeat original Link 
+      if (results.length > 0) {
+        shortURL = results[0]['shortLink']
+        res.render('result', { shortURL: shortURL, originalLink: originalLink })
+      } else {
+        link.save(err => {
+          if (err) return console.log(err)
+          res.render('result', { shortURL: shortURL, originalLink: originalLink })
+        })
+      }
+    })
   })
+
 })
 
 module.exports = router
