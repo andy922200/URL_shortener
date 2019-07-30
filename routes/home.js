26cec663065d4d5875df7b8667e8c9e0a880af30
andysml <andy922200@gmail.com>
2019-07-30 20:36:44 +0100

    add redirect function

--- routes/home.js
+++ routes/home.js
@@ -2,11 +2,22 @@
 // initialize router
 const express = require('express')
 const router = express.Router()
-//const Link = require('../models/link.js')
+const Link = require('../models/link.js')
 
 // index router
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+// redirect shortURL to originalUrl
+router.get(/[0-9A-Za-z]{5}/, (req, res) => {
+  const baseUrl = 'http://localhost:3000'
+  let shortLink = baseUrl + req.url
+  let originalLink = ''
+  Link.find({ "shortLink": shortLink }, (err, result) => {
+    originalLink = result[0].link
+    res.redirect(originalLink)
+  })
 })
 
 module.exports = router
