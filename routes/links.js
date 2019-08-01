9b76514b29388dafea62989c876f03ca8e2a4414
andysml <andy922200@gmail.com>
2019-08-01 05:47:24 +0100

    update code styld in link.js

--- routes/links.js
+++ routes/links.js
@@ -8,13 +8,18 @@ const formCheck = require('../models/validationRule')
 
 // send original Link to tinyURL proxy
 router.post('/', formCheck, async (req, res) => {
+  let originalLink = req.body.originalLink
   const errors = validationResult(req)
   //const baseURL = "http://localhost:3000/"
   const baseURL = "https://mighty-river-85810.herokuapp.com/"
   let randomString = Math.random().toString(36).slice(-5)
-  let originalLink = req.body.originalLink
   let shortURL = ''
   shortURL = baseURL + randomString
+  const link = Link({
+    link: originalLink,
+    shortLink: shortURL
+  })
+
   if (!errors.isEmpty()) {
     let errorMessages = []
     console.log(errors)
@@ -24,37 +29,32 @@ router.post('/', formCheck, async (req, res) => {
       //console.log(errorMessages)
     }
     res.render('index', { errorMessages: errorMessages })
-  } else {
-    try {
-      let results = await Link.find({ 'shortLink': shortURL }).exec()
-      while (results.length > 0) {
-        randomString = Math.random().toString(36).slice(4, 9)
-        shortURL = baseURL + randomString
-        results = await Link.find({ 'shortLink': shortURL }).exec()
-      }
-      Link.find({ "link": originalLink })
-        .exec((err, results) => {
-          const link = Link({
-            link: originalLink,
-            shortLink: shortURL
-          })
-          if (err) return err
-          // validate repeat original Link
-          if (results.length > 0) {
-            shortURL = results[0]['shortLink']
-            res.render('result', { shortURL: shortURL, originalLink: originalLink })
-          } else {
-            link.save(err => {
-              if (err) return console.log(err)
-              res.render('result', { shortURL: shortURL, originalLink: originalLink })
-            })
-          }
-        })
-        .catch((err) => console.log(err))
-    } catch (error) {
-      console.log(error)
-    }
   }
+  try {
+    let results = await Link.find({ 'shortLink': shortURL }).exec()
+    while (results.length > 0) {
+      randomString = Math.random().toString(36).slice(-5)
+      shortURL = baseURL + randomString
+      results = await Link.find({ 'shortLink': shortURL }).exec()
+    }
+    Link.find({ "link": originalLink })
+      .exec((err, results) => {
+        if (err) return err
+        // validate repeat original Link
+        if (results.length > 0) {
+          shortURL = results[0]['shortLink']
+          res.render('result', { shortURL: shortURL, originalLink: originalLink })
+        } else {
+          link.save(err => {
+            if (err) return console.log(err)
+            res.render('result', { shortURL: shortURL, originalLink: originalLink })
+          })
+        }
+      })
+  } catch (error) {
+    console.log(error)
+  }
+
 })
 
 module.exports = router
